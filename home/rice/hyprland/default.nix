6dc10f2efe4390cdf484cac89b6b37815dd240d4
Tiebe Groosman <tiebe.groosman@gmail.com>
2024-03-03 01:26:09 +0100

    first commit

---
+++ home/rice/hyprland/default.nix
@@ -0,0 +1,55 @@
+{
+  pkgs,
+  lib,
+  inputs,
+  theme,
+  ...
+}:
+with lib; let
+  mkService = lib.recursiveUpdate {
+    Unit.PartOf = ["graphical-session.target"];
+    Unit.After = ["graphical-session.target"];
+    Install.WantedBy = ["graphical-session.target"];
+  };
+in {
+  imports = [./config.nix ./binds.nix ./rules.nix];
+  home.packages = with pkgs;
+  with inputs.hyprcontrib.packages.${pkgs.system}; [
+    libnotify
+    wf-recorder
+    brightnessctl
+    pamixer
+    python39Packages.requests
+    slurp
+    grim
+    hyprpicker
+    swappy
+    grimblast
+    hyprpicker
+    wl-clip-persist
+    wl-clipboard
+    pngquant
+    cliphist
+  ];
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+    package = inputs.hyprland.packages.${pkgs.system}.default;
+    systemd = {
+      variables = ["--all"];
+      extraCommands = [
+        "systemctl --user stop graphical-session.target"
+        "systemctl --user start hyprland-session.target"
+      ];
+    };
+  };
+
+  # fake a tray to let apps start
+  # https://github.com/nix-community/home-manager/issues/2064
+  systemd.user.targets.tray = {
+    Unit = {
+      Description = "Home Manager System Tray";
+      Requires = ["graphical-session-pre.target"];
+    };
+  };
+}
