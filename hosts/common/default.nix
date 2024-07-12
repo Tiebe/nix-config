6f4b9e2ef8f58a725e9d6e1bb9054af39a6fa946
Tiebe Groosman <tiebe.groosman@gmail.com>
2024-07-12 12:33:44 +0200

    test

--- hosts/common/default.nix
+++ hosts/common/default.nix
@@ -131,7 +131,7 @@
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
-  services.displayManager.sddm.enable = true;
+  services.displayManager.gdm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
   environment.plasma6.excludePackages = with pkgs.kdePackages; [
