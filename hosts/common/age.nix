b5db25c9189b56e65043375ee5571e3a418e06d5
Tiebe Groosman <tiebe.groosman@gmail.com>
2024-08-06 11:01:48 +0000

    Age stuff

--- hosts/common/age.nix
+++ hosts/common/age.nix
@@ -16,9 +16,12 @@ in {
     yubikey-personalization
     yubikey-personalization-gui
     yubikey-manager
+    gawk
+    gnupg
     inputs.agenix.packages.x86_64-linux.default
   ];
 
+  programs.gnupg.agent.enable = true;
   services.yubikey-agent.enable = true;
   services.pcscd.enable = true;
 
@@ -29,7 +32,48 @@ in {
     };
 
     identityPaths = [
-      (agePath "keys/age-yubikey-identity-c67fa313.txt")
+      (lib.mkIf (builtins.getEnv "HOSTNAME" != "jupiter") (agePath "keys/age-yubikey-identity-c67fa313.txt"))
+      (lib.mkIf (builtins.getEnv "HOSTNAME" == "jupiter") (agePath "keys/age-yubikey-identity-9b188c32.txt"))
     ];
   };
+
+  # system.activationScripts.waitForYubikey = {
+  #   text = ''
+  #     source ${config.system.build.setEnvironment}
+  #     PRESENT=true
+  #     echo Waiting for Yubikey...
+
+  #     while $PRESENT
+  #     do
+  #         RESULT=`2>&1 gpg2 --card-status`
+  #         echo $RESULT
+  #         CARD_ABSENT=`echo $RESULT | grep "OpenPGP card not available"`
+  #         if [ -z "''${CARD_ABSENT}" ]
+  #         then
+  #             SAVEIFS=$IFS   # Save current IFS (Internal Field Separator)
+  #             IFS=$'\n'      # Change IFS to newline char
+  #             lines=($RESULT)
+  #             IFS=$SAVEIFS   # Restore original IFS
+  #             SIGNATURE=`echo ''${lines[17]} | awk -F":" '{print $2}'`
+  #             if [ -z "''${SIGNATURE}" ]
+  #             then
+  #                 PRESENT=true
+  #                 sleep 1
+  #             else
+  #                 echo Yubikey found!
+  #                 PRESENT=false
+  #             fi
+  #         else
+  #              PRESENT=true
+  #              sleep 1
+  #         fi
+  #     done
+  #   '';
+  # };
+
+  system.activationScripts.agenixInstall.deps = [
+    "agenixNewGeneration"
+    "specialfs"
+    #"waitForYubikey"
+  ];
 }
