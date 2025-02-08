8a47d13007e041102febd3c56f011fd83a31a397
Tiebe Groosman <tiebe.groosman@gmail.com>
2025-02-08 22:12:48 +0100

    Remove vencord overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -4,14 +4,5 @@
     ragenix = prev.ragenix.override {
       plugins = [final.age-plugin-yubikey];
     };
-
-    vencord = prev.vencord.overrideAttrs (old: {
-      src = prev.fetchFromGitHub {
-        owner = builtins.trace "test" "Tiebe";
-        repo = "Vencord";
-        rev = "v${prev.version}";
-        hash = "";
-      };
-    });
   };
 }
