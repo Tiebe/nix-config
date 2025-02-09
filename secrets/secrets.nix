67093b5a59076d8e2a4b11a6863e4722182dc80e
Tiebe Groosman <tiebe.groosman@gmail.com>
2025-02-09 11:18:07 +0100

    Change secrets to also use ssh host keys

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,10 +2,12 @@ let
   yubikey_5c = "age1yubikey1qgjdwvurkzt96kq0ru2wwvpemcpemc2xdldxaas4fv6kf5w5s820ql6mvra";
   yubikey_5 = "age1yubikey1qtz92pnpac4tgeehratk52syxylpwftlujl8ey5curz4h62mam4kyw5jlsd";
 
+  jupiter = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFgt8r0Bw2Zikcjz4NPvty826oHnznFyBtJFK1ngNVXS";
+
   yubikeys = [yubikey_5c yubikey_5];
 in {
-  "password.age".publicKeys = [yubikey_5c];
-  "tailscale.age".publicKeys = [yubikey_5c];
-  "wifi.age".publicKeys = [yubikey_5c];
-  "atuin.age".publicKeys = [yubikey_5c];
+  "password.age".publicKeys = [yubikey_5c jupiter];
+  "tailscale.age".publicKeys = [yubikey_5c jupiter];
+  "wifi.age".publicKeys = [yubikey_5c jupiter];
+  "atuin.age".publicKeys = [yubikey_5c jupiter];
 }
