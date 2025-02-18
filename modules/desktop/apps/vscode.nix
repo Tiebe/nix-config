743654cae0ea72b27f08656ec91c3626848e9112
Tiebe Groosman <tiebe.groosman@gmail.com>
2025-02-18 17:31:23 +0100

    VSCode

--- modules/desktop/apps/vscode.nix
+++ modules/desktop/apps/vscode.nix
@@ -16,9 +16,24 @@ in {
   };
 
   config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [ nil ];
+
     home-manager.users.tiebe = {
       programs.vscode = {
         enable = true;
+        extensions = with pkgs.vscode-extensions; [
+          ms-python.python ms-python.pylint ms-python.vscode-pylance
+          ms-azuretools.vscode-docker
+          jock.svg
+          jnoortheen.nix-ide 
+        ];
+
+        userSettings = {
+          "nix.enableLanguageServer" = true;
+          "[nix]"."editor.tabSize" = 2;
+          "files.autoSave" = "on";
+        };
+
       };
     };
   };
