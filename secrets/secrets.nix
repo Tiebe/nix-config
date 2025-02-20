dcff0e42ff59fc9281b5687450c2b1a24b43ef2a
Tiebe Groosman <tiebe.groosman@gmail.com>
2025-02-20 10:25:47 +0100

    Add pluto secret ssh key

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,11 +3,12 @@ let
   yubikey_5 = "age1yubikey1qtz92pnpac4tgeehratk52syxylpwftlujl8ey5curz4h62mam4kyw5jlsd";
 
   jupiter = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFgt8r0Bw2Zikcjz4NPvty826oHnznFyBtJFK1ngNVXS";
+  pluto = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDEvOgR7VedJwRvKO9wD8am7K388emFAgMk31NMzn2di";
 
   yubikeys = [yubikey_5c yubikey_5];
 in {
-  "password.age".publicKeys = [yubikey_5c jupiter];
-  "tailscale.age".publicKeys = [yubikey_5c jupiter];
-  "wifi.age".publicKeys = [yubikey_5c jupiter];
-  "atuin.age".publicKeys = [yubikey_5c jupiter];
+  "password.age".publicKeys = [yubikey_5c jupiter pluto];
+  "tailscale.age".publicKeys = [yubikey_5c jupiter pluto];
+  "wifi.age".publicKeys = [yubikey_5c jupiter pluto];
+  "atuin.age".publicKeys = [yubikey_5c jupiter pluto];
 }
