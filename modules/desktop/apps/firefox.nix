1ed502fe048d520c9528b1c578b6fbf925a5f1ae
Tiebe Groosman <tiebe.groosman@gmail.com>
2025-03-05 11:44:38 +0100

    Remove i dont care about cookies

--- modules/desktop/apps/firefox.nix
+++ modules/desktop/apps/firefox.nix
@@ -82,11 +82,6 @@ in {
               install_url = "https://addons.mozilla.org/firefox/downloads/latest/studytools/latest.xpi";
               installation_mode = "force_installed";
             };
-            # I don't care about cookies
-            "jid1-KKzOGWgsW3Ao4Q@jetpack" = {
-              install_url = "https://addons.mozilla.org/firefox/downloads/latest/i-dont-care-about-cookies/latest.xpi";
-              installation_mode = "force_installed";
-            };
             # Enhanced GitHub
             "{72bd91c9-3dc5-40a8-9b10-dec633c0873f}" = {
               install_url = "https://addons.mozilla.org/firefox/downloads/latest/enhanced-github/latest.xpi";
@@ -128,6 +123,19 @@ in {
             Extensions = {
               "{446900e4-71c2-419f-a6a7-df9c091e268b}".environment = {
                 base = "https://bitwarden.groosman.nl";
+              };
+
+              "uBlock0@raymondhill.net" = {
+                disableFirstRunPage = true;
+                rulesets = [
+                  "-*"
+                  "+default"
+                  "adguard-cookies"
+                  "ublock-cookies-adguard"
+                  "NLD-0"
+                  "adguard-spyware"
+                  "adguard-spyware-url"
+                ];
               };
             };
           };
