6904d80e92bb2e722b8ea7d276392e954e1d12a1
Pi (OpenClaw) <pi@openclaw>
2026-04-04 22:31:31 +0200

    fix(desktop): persist Discord config with evict-darlings support
    Add evict-darlings branch to use baseDir-relative persist path when
    evictDarlings is enabled, and add home.activation to pre-create the
    /persist target directory before symlinks are set up.

--- modules/desktop/apps/discord/darlings.nix
+++ modules/desktop/apps/discord/darlings.nix
@@ -1,17 +1,35 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: let
   inherit (lib) mkIf;
   cfg = config.tiebe.desktop.apps.discord;
   darlings = config.tiebe.system.boot.darlings;
+  evictCfg = config.tiebe.system.boot.evictDarlings;
 in {
   config = mkIf (darlings.enable && cfg.enable) {
     # Discord config persistence - use home-manager's config.lib.file
-    home-manager.users.tiebe = {config, ...}: {
+    home-manager.users.tiebe = {
+      config,
+      lib,
+      ...
+    }: {
       home.file.".config/discord".source =
-        config.lib.file.mkOutOfStoreSymlink "/persist/home/tiebe/.config/discord";
+        if evictCfg.enable
+        then config.lib.file.mkOutOfStoreSymlink "/persist${evictCfg.baseDir}/.config/discord"
+        else config.lib.file.mkOutOfStoreSymlink "/persist/home/tiebe/.config/discord";
+
+      # Create the target directories in /persist before symlinks are set up
+      home.activation.createDiscordPersistDirs = lib.hm.dag.entryBefore ["writeBoundary"] ''
+        $DRY_RUN_CMD ${pkgs.coreutils}/bin/mkdir -p $VERBOSE_ARG \
+          ${
+          if evictCfg.enable
+          then ''"/persist${evictCfg.baseDir}/.config/discord"''
+          else ''"/persist/home/tiebe/.config/discord"''
+        }
+      '';
     };
   };
 }
