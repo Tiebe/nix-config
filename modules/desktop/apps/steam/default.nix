961510f10aab613353cfd9296a36af93cec6cc78
Tiebe Groosman <tiebe@tiebe.me>
2026-06-15 11:29:44 +0200

    feat(steam): add AlecaFrame desktop entry

--- modules/desktop/apps/steam/default.nix
+++ modules/desktop/apps/steam/default.nix
@@ -20,6 +20,35 @@
         })
       ];
   });
+
+  protonhax = pkgs.stdenv.mkDerivation rec {
+    pname = "protonhax";
+    version = "1.0.5";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "jcnils";
+      repo = pname;
+      rev = version;
+      sha256 = "sha256-5G4MCWuaF/adSc9kpW/4oDWFFRpviTKMXYAuT2sFf9w=";
+    };
+
+    installPhase = ''
+      mkdir -p $out/bin
+      cp protonhax $out/bin
+    '';
+
+    postFixup = ''
+      sed -i '1s/#!/#!${lib.escape ["/"] (lib.getExe pkgs.steam-run)} /' $out/bin/protonhax
+    '';
+
+    meta = {
+      description = "Tool to help running other programs (i.e. Cheat Engine) inside Steam's proton";
+      homepage = "https://github.com/jcnils/protonhax";
+      license = lib.licenses.bsd3;
+      maintainers = [lib.maintainers.pneg];
+      mainProgram = "protonhax";
+    };
+  };
 in {
   imports = [
     ./darlings.nix
@@ -32,6 +61,28 @@ in {
   };
 
   config = mkIf cfg.enable {
+    home-manager.users.tiebe = let
+      homeRoot =
+        if evictCfg.enable
+        then evictCfg.configDir
+        else "/home/tiebe";
+      steamDataRoot = "${homeRoot}/.local/share/Steam";
+      compatPrefix = "${steamDataRoot}/steamapps/compatdata/230410/pfx";
+    in {
+      xdg.desktopEntries.alecaframe = {
+        name = "AlecaFrame";
+        exec = ''protonhax run 230410 "${compatPrefix}/drive_c/users/steamuser/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Overwolf/AlecaFrame.lnk"'';
+        type = "Application";
+        startupNotify = true;
+        icon = "${compatPrefix}/drive_c/users/steamuser/AppData/Local/Overwolf/AppShortcutIcons/afmcagbpgggkpdkokjhjkllpegnadmkignlonpjm.ico";
+        terminal = false;
+        settings = {
+          "Name[en_US]" = "AlecaFrame";
+          TerminalOptions = "";
+        };
+      };
+    };
+
     programs.steam = {
       enable = true;
       remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
@@ -51,6 +102,6 @@ in {
         else pkgs.steam;
     };
 
-    environment.systemPackages = with pkgs; [gamescope gamemode bubblewrap];
+    environment.systemPackages = with pkgs; [gamescope gamemode bubblewrap protonhax];
   };
 }
